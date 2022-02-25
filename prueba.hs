c6b525f77199af03056713cdfcfd4d560c8f38ad
Emmanuel Sanchez <fede@fede.website>
2022-02-25 07:49:40 -0600

    testing Strings

--- prueba.hs
+++ prueba.hs
@@ -2,3 +2,6 @@ prueba = print("Hola programador")
 galleta = print("elmo quiere una galleta")
 peras = 7 +3 -1
 --operadores logicos
+cadena1 = "Hola soy una cadena"
+cadena2 = "o no"
+cadena = cadena1 ++ " " ++ cadena2
