3816b19a2fa59d21496e8656b6fac5a74978bc72
Emmanuel Sanchez <fede@fede.website>
2022-03-11 22:41:58 -0600

    working with functions

--- prueba.hs
+++ prueba.hs
@@ -9,3 +9,23 @@ cadena = cadena1 ++ " " ++ cadena2
 numeroalgo = succ 7
 
 minnumber = min 5 ( min 4 3 )
+
+sumar a b = a+b
+
+sumar100 x = x + 100
+
+parImpar x = if x `mod` 2 == 0 then "par" else "impar"
+
+mayorTres a b c = if a >= b && a >= c
+    then  a
+    else if b>= a && b >= c then
+        b
+        else
+            c
+
+divisible x y = if x `mod` y == 0 then
+    "Divisibles"
+    else
+        "no Divisivles"
+
+otracosa x = "Usted envio "++(show x)
